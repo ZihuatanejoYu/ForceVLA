ee91fe6c01cc8aa608ffac071aba80a2d86ef859
hairuoliu1 <hairuoliu@sjtu.edu.cn>
2025-07-28 08:54:05 +0800

    Update

--- uv_run_server.sh
+++ uv_run_server.sh
@@ -1,7 +1,7 @@
-cd /home/qiaojun/flexiv_manipulation/example_py/flexiv_pi0-dev
+# cd /home/qiaojun/flexiv_manipulation/example_py/flexiv_pi0-dev
 source .venv/bin/activate
 
-export HF_HUB_OFFLINE=1
+# export HF_HUB_OFFLINE=1
 uv run scripts/serve_policy.py policy:checkpoint \
-    --policy.config=pi0_fast_flexiv_noforce_lora \
-    --policy.dir=/data_1/flexiv_pi0_ckpts/pi0_fast_noforce/insert_plug/10000
+    --policy.config=pi0_guidance_lora \
+    --policy.dir=/data/hairuo/checkpoints/pi0/flexiv/pi0_guidance_lora/pi0_guidance_rebuttle_force_ablation_arch_insert_plug_0725/10000
